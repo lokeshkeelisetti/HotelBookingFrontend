94c66fd70d3daabe73fb2fedf03420db4818de0e
lokesh keelisetti <keelisettilokesh@gmail.com>
2021-05-07 07:35:28 +0530

    added Login and Registering Using Modal

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import {Navbar,NavbarToggler,NavbarBrand,Collapse,Nav,NavItem,NavLink,Button} from 'reactstrap';
+import {Navbar,NavbarToggler,NavbarBrand,Collapse,Nav,NavItem,NavLink,Button, 
+    Modal, ModalHeader, ModalBody, FormGroup, Form,Input,Label} from 'reactstrap';
 
 class Header extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            isNavOpen : false
+            isNavOpen : false,
+            isModalOpen : false,
+            isLogin : true
         };
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.toggleLogin = this.toggleLogin.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    toggleLogin(){
+        this.setState({
+            isLogin : !this.state.isLogin
+        })
     }
 
     toggleNav(){
         this.setState({
             isNavOpen : !this.state.isNavOpen
         })
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen : !this.state.isModalOpen
+        })
+    }
+
+    handleLogin(event){
+        event.preventDefault();
+        this.toggleModal();
+        if(this.state.isLogin){
+            alert('Username: '+this.username.value+' Password: '+this.password.value);
+        }
+        else{
+            alert('Firstname: '+this.firstname.value+ ' Lastname: '+this.lastname.value);
+        }
     }
 
     render(){
@@ -25,6 +54,7 @@ class Header extends Component{
                         <NavbarBrand className="mr-auto ml-2" href="/">
                             <img src="/logo192.png" height="30" width="41"
                                 alt="Logo" />
+                                Pack Your Bags
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                         <Nav navbar className = "ml-auto mr-2">
@@ -50,6 +80,45 @@ class Header extends Component{
                         </Nav>
                         </Collapse>
                 </Navbar>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        {this.state.isLogin ? 'Login' : 'Register'}
+                    </ModalHeader>
+                   {this.state.isLogin ? ( <ModalBody>
+                        <Form onSubmit = {this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="email">Email</Label> 
+                                <Input type="email" id="email" name="email"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef = {(password) => this.password = password} />
+                            </FormGroup>
+                            <Button type="submit" value="submit" className="bg-primary" color="primary">Login</Button>
+                            <Button onClick={this.toggleLogin} color="link">Register</Button>
+                        </Form>
+                    </ModalBody>) : (
+                        <ModalBody>
+                            <Form onSubmit = {this.handleLogin}>
+                                <FormGroup>
+                                    <Label htmlFor="firstname">First Name</Label>
+                                    <Input type="text" id="firstname" name="firstname"
+                                        innerRef={(firstname) => this.firstname = firstname} />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label htmlFor="lastname">Last Name</Label>
+                                    <Input type="text" id="lastname" name="lastname"
+                                        innerRef={(lastname) => this.lastname = lastname} />
+                                </FormGroup>
+                                <Button type="submit" value="submit" className="bg-primary" color="primary">Register</Button>
+                                <Button onClick={this.toggleLogin} color="link">Already have a account</Button>
+                            </Form>
+                        </ModalBody>
+                    )
+                    }
+                </Modal>
             </React.Fragment>
         )
     }
