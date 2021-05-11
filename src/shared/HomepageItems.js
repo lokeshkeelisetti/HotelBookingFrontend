ee1a272eae51620a3bd9636aa4b3982bc3b724fb
lokesh keelisetti <keelisettilokesh@gmail.com>
2021-05-11 17:08:22 +0530

    changed home component to functional component

---
+++ src/shared/HomepageItems.js
@@ -0,0 +1,19 @@
+let items = [
+	{
+		src: "assets/images/hotel1.jpeg",
+		altText: "Hotel",
+		caption: "Hotel",
+	},
+	{
+		src: "assets/images/hotel2.jpeg",
+		altText: "Another Hotel",
+		caption: "Another Hotel",
+	},
+	{
+		src: "assets/images/hotel3.jpeg",
+		altText: "Again Hotel",
+		caption: "Again Hotel",
+	},
+];
+
+export default items;
